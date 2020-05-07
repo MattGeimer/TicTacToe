c79c1b798753cb6ac59e2098b5bb767d441e5638
Matt Geimer <matasgeimer@gmail.com>
2020-05-07 15:47:57 -0500

    [chore] Add comments to allTests.sh

--- allTests.sh
+++ allTests.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
+# This file is used to run all tests locally for all devices (instead of using precious github actions minutes)
 
-echo Starting Unit Tests
-
-declare -a devices=('platform=iOS Simulator,OS=13.4.1,name=iPhone 8', 'platform=iOS Simulator,OS=13.4.1,name=iPhone 11 Pro')
+echo Starting build
 
 xcodebuild clean build-for-testing -scheme "EZ Tic Tac Toe" \
 -destination 'platform=iOS Simulator,name=iPhone 11' \
@@ -17,6 +16,9 @@ xcodebuild clean build-for-testing -scheme "EZ Tic Tac Toe" \
 -destination 'platform=iOS Simulator,name=iPad Pro (12.9-inch) (3rd generation)' \
 -destination 'platform=iOS Simulator,name=iPad Pro (12.9-inch) (4th generation)' \
 -destination 'platform=iOS Simulator,name=iPad Pro (9.7-inch)'
+
+echo Starting test
+
 xcodebuild test-without-building -scheme "EZ Tic Tac Toe" \
 -destination 'platform=iOS Simulator,name=iPhone 11' \
 -destination 'platform=iOS Simulator,name=iPhone 11 Pro' \
