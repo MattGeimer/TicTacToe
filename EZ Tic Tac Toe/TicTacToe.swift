792956d086f3011f52b8091759e44e1f8b20666c
Matt Geimer <mgeimer2@illinois.edu>
2021-06-12 15:49:02 -0700

    Convert to use test plans and update for new devices

--- EZ Tic Tac Toe/TicTacToe.swift
+++ EZ Tic Tac Toe/TicTacToe.swift
@@ -12,7 +12,7 @@ import SwiftUI
 struct TicTacToe: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+			ContentView()
         }
     }
 }
--- allTests.sh
+++ allTests.sh
@@ -4,27 +4,41 @@
 echo Starting build
 
 xcodebuild clean build-for-testing -scheme "EZ Tic Tac Toe" \
+-destination 'platform=iOS Simulator,name=iPad (8th generation)' \
+-destination 'platform=iOS Simulator,name=iPad Air (4th generation)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (9.7-inch)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (11-inch) (2nd generation)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (11-inch) (3rd generation)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (12.9-inch) (4th generation)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (12.9-inch) (5th generation)' \
+-destination 'platform=iOS Simulator,name=iPhone 8' \
+-destination 'platform=iOS Simulator,name=iPhone 8 Plus' \
 -destination 'platform=iOS Simulator,name=iPhone 11' \
 -destination 'platform=iOS Simulator,name=iPhone 11 Pro' \
 -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max' \
--destination 'platform=iOS Simulator,name=iPhone 8' \
--destination 'platform=iOS Simulator,name=iPhone 8 Plus' \
--destination 'platform=iOS Simulator,name=iPhone SE (2nd generation)' \
--destination 'platform=iOS Simulator,name=iPad Air (3rd generation)' \
--destination 'platform=iOS Simulator,name=iPad Pro (11-inch) (2nd generation)' \
--destination 'platform=iOS Simulator,name=iPad Pro (12.9-inch) (4th generation)' \
--destination 'platform=iOS Simulator,name=iPad Pro (9.7-inch)'
+-destination 'platform=iOS Simulator,name=iPhone 12' \
+-destination 'platform=iOS Simulator,name=iPhone 12 Pro' \
+-destination 'platform=iOS Simulator,name=iPhone 12 Pro Max' \
+-destination 'platform=iOS Simulator,name=iPhone 12 mini' \
+-destination 'platform=iOS Simulator,name=iPhone SE (2nd generation)'
 
 echo Starting test
 
 xcodebuild test-without-building -scheme "EZ Tic Tac Toe" \
+-destination 'platform=iOS Simulator,name=iPad (8th generation)' \
+-destination 'platform=iOS Simulator,name=iPad Air (4th generation)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (9.7-inch)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (11-inch) (2nd generation)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (11-inch) (3rd generation)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (12.9-inch) (4th generation)' \
+-destination 'platform=iOS Simulator,name=iPad Pro (12.9-inch) (5th generation)' \
+-destination 'platform=iOS Simulator,name=iPhone 8' \
+-destination 'platform=iOS Simulator,name=iPhone 8 Plus' \
 -destination 'platform=iOS Simulator,name=iPhone 11' \
 -destination 'platform=iOS Simulator,name=iPhone 11 Pro' \
 -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max' \
--destination 'platform=iOS Simulator,name=iPhone 8' \
--destination 'platform=iOS Simulator,name=iPhone 8 Plus' \
--destination 'platform=iOS Simulator,name=iPhone SE (2nd generation)' \
--destination 'platform=iOS Simulator,name=iPad Air (3rd generation)' \
--destination 'platform=iOS Simulator,name=iPad Pro (11-inch) (2nd generation)' \
--destination 'platform=iOS Simulator,name=iPad Pro (12.9-inch) (4th generation)' \
--destination 'platform=iOS Simulator,name=iPad Pro (9.7-inch)'
+-destination 'platform=iOS Simulator,name=iPhone 12' \
+-destination 'platform=iOS Simulator,name=iPhone 12 Pro' \
+-destination 'platform=iOS Simulator,name=iPhone 12 Pro Max' \
+-destination 'platform=iOS Simulator,name=iPhone 12 mini' \
+-destination 'platform=iOS Simulator,name=iPhone SE (2nd generation)'
